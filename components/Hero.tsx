66cba66c10e8b311f42e9b3d38ef9c1db37f6e6c
YxngXi1 <xuetianyang@gmail.com>
2024-11-19 22:34:04 -0500

    initial initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='min-h-screen flex justify-center items-center'>
+        <div className='container'>
+            <div className='grid grid-cols-2 justify-center'>
+                <div className='mx-auto'>
+                    Picture or something goes here
+                </div>
+                <div className='mx-auto'>
+                    Nathan Tsegaye
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
